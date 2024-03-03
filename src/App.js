83401322dbfe0e7c5a6ce87bf661f1c46652473c
Manoj.A <manojamir2911@gmail.com>
2024-03-03 10:45:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Mainpage from "./components/Mainpage/Mainpage";
+function App() {
+  return (
+    <div className="container">
+      <Mainpage />
+    </div>
+  );
+}
+
+export default App;
